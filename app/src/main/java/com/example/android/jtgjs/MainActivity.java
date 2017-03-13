1155f76fd0a6da539f193d2938d036bd0e2cb421
swati-93 <swati.1993cs@gmail.com>
2017-03-13 16:28:39 +0530

    Second Commit collapsing toolbar cardview done

---
+++ app/src/main/java/com/example/android/jtgjs/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.jtgjs;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar= (Toolbar)findViewById(R.id.MyToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapse_toolbar);
+        collapsingToolbarLayout.setTitle("JTGJS");
+    }
+}
